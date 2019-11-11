2121a7a4d6cf966375e51b9f7eb1e200c7f0c5b6
RafaelRodrigues446 <rodriguesrafael446@gmail.com>
2019-11-11 15:09:19 -0200

    Final commits

--- app/src/main/java/br/com/senai/newsapp/CategoriesActivity.java
+++ app/src/main/java/br/com/senai/newsapp/CategoriesActivity.java
@@ -1,5 +1,8 @@
 package br.com.senai.newsapp;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 public class CategoriesActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,22 @@ public class CategoriesActivity extends AppCompatActivity {
         toolbar.setTitle("Categories");
         setSupportActionBar(toolbar);
 
+
+        //Criando o botão do drawer
+        drawerLayout = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
